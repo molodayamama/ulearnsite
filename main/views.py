25cdc78b871b7ab6c4cc5289cd4ab46d7a7aab46
molodaya_mama <115584804+molodayamama@users.noreply.github.com>
2025-01-13 02:28:13 +0500

    Add views; remove config

--- main/views.py
+++ main/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+def general_statistics(request):
+    return render(request, 'main/general_statistics.html')
+
+def demand(request):
+    return render(request, 'main/demand.html')
+
+def geography(request):
+    return render(request, 'main/geography.html')
+
+def skills(request):
+    return render(request, 'main/skills.html')
+
+def latest_vacancies(request):
+    return render(request, 'main/latest_vacancies.html')
